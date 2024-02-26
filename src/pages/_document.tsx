6931e12717efbe2293f88e6e14374f4aa6e30260
thomas1996 <thomas.kantahck@gmail.com>
2024-02-26 23:16:19 +0100

    adding buttons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,8 +9,8 @@ export default function Document() {
         <meta property="og:image" content="https://svgshare.com/i/13fG.svg"/>
         <meta property="fc:frame:button:1" content="Team A"/>
         <meta property="fc:frame:button:2" content="Team B"/>
-        <meta property="fc:frame:button:2" content="Summary"/>
-        <meta property="fc:frame:button:2" content="Exit"/>
+        <meta property="fc:frame:button:3" content="Summary"/>
+        <meta property="fc:frame:button:4" content="Exit"/>
       </Head>
       <body>
         <Main />
