f4c4f6302fcb810c36cfca8ba4d4b54fa0b5f3be
thomas1996 <thomas.kantahck@gmail.com>
2024-02-26 23:11:02 +0100

    adding new hosted link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="https://drive.google.com/file/d/1S972OBEMLe7RINrorDAPtWL2qop1Ex3R/view?usp=drive_link"/>
-        <meta property="og:image" content="https://drive.google.com/file/d/1S972OBEMLe7RINrorDAPtWL2qop1Ex3R/view?usp=drive_link"/>
+        <meta property="fc:frame:image" content="https://svgshare.com/s/13fG"/>
+        <meta property="og:image" content="https://svgshare.com/s/13fG"/>
         <meta property="fc:frame:button:1" content="Team A"/>
         <meta property="fc:frame:button:2" content="Team B"/>
         <meta property="fc:frame:button:2" content="Summary"/>
