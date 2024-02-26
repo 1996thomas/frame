dccd33d87f9e09bb4db0b40a53535f44573851ce
thomas1996 <thomas.kantahck@gmail.com>
2024-02-26 23:12:19 +0100

    oops i forget .svg

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="https://svgshare.com/s/13fG"/>
-        <meta property="og:image" content="https://svgshare.com/s/13fG"/>
+        <meta property="fc:frame:image" content="https://svgshare.com/s/13fG.svg"/>
+        <meta property="og:image" content="https://svgshare.com/i/13fG.svg"/>
         <meta property="fc:frame:button:1" content="Team A"/>
         <meta property="fc:frame:button:2" content="Team B"/>
         <meta property="fc:frame:button:2" content="Summary"/>
