d65a76e4c3ec817e1a7ba945c5329ee439b8e317
thomas1996 <thomas.kantahck@gmail.com>
2024-02-26 22:11:19 +0100

    adding meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://picsum.photos/200"/>
+        <meta property="og:image" content="https://picsum.photos/200"/>
+        <meta property="fc:frame:button:1" content="Coucou"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
